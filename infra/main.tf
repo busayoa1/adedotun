13213d4be50bee570e8a11e914f708217f9b2553
busayoa1 <93959843+busayoa1@users.noreply.github.com>
2022-11-11 15:07:35 -0500

    first commit

---
+++ infra/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = "~> 4.3.0"
+  }
+
+  backend "s3" {
+    bucket = "olufolakeadebiyi"
+    key    = "test/app-stack.tfstate"
+    region = "us-east-2"
+  }
+}
+
+provider "aws" {
+  region = var.AWS_REGION
+}
